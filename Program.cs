f7efe89841249749d0c68bf547d36858d49feb51
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:27:51 -0300

    exercicio feito?

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿decimal gasolina, etanol;
+const decimal percentualRecomendado = (73);
+decimal percentualConta;
+
+Console.Clear();
+
+Console.WriteLine("--- Etanol ou Gasolina? ---\n");
+
+Console.Write("Digite o preço do etanol (R$): ");
+etanol = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o preço da gasolina (R$): ");
+gasolina = Convert.ToDecimal(Console.ReadLine());
+
+percentualConta = ((etanol / gasolina) * 100);
+
+Console.WriteLine($"O preço do etanol corresponde a {percentualConta:N1} do preço da gasolina");
+
+if(percentualConta <= percentualRecomendado)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("Recomendação: Abasteça com ETANOL.");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Recomendação: Abasteça com GASOLINA.");
+    Console.ResetColor();
+}
